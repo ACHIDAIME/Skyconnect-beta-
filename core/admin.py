c3548dd43d8fc292060edd31460d74efd8d350a7
ACHIDAIME <mirabeautoureend@gmail.com>
2025-08-12 15:12:18 +0000

    message

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Service, MessageContact, ZoneCouverture,Slider,Actualite
+from .models import Service, MessageContact, ZoneCouverture,Slider,Actualite,Logo
 
 admin.site.register(Service)
 admin.site.register(MessageContact)
 admin.site.register(ZoneCouverture)
 admin.site.register(Slider)
 admin.site.register(Actualite)
+admin.site.register(Logo)
--- core/models.py
+++ core/models.py
@@ -7,20 +7,20 @@ from django.db import models
 
 class Service(models.Model):
     nom = models.CharField(max_length=100)
-    description = models.TextField()
-    icone = models.CharField(
-        max_length=50,
-        help_text="Nom de l’icône Bootstrap ex: 'bi-wifi'",
-        default='bi-wifi'
-    )
-    prix = models.CharField(max_length=50, default='À définir')
+    icone = models.CharField(max_length=50, help_text="Nom de l’icône Bootstrap ex: 'bi-wifi'", default='bi-wifi')
+    description1 = models.CharField("Description courte", max_length=255, blank=True)
+    description2 = models.CharField("Avantage 1", max_length=255, blank=True)
+    description3 = models.CharField("Avantage 2", max_length=255, blank=True)
+    description4 = models.CharField("Avantage 3", max_length=255, blank=True)
+    prix = models.CharField(max_length=50, blank=True, null=True)
     badge = models.CharField(max_length=30, blank=True, help_text="Ex: Populaire, Service+, 24/7")
     badge_color = models.CharField(max_length=30, blank=True, help_text="Ex: badge-promo")
     ordre = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.nom
-
+    
+    
 class MessageContact(models.Model):
     nom = models.CharField(max_length=100)
     email = models.EmailField()
@@ -58,3 +58,10 @@ class Actualite(models.Model):
 
     def __str__(self):
         return self.titre
+
+class Logo(models.Model):
+    image = models.ImageField(upload_to='logo/')
+    alt = models.CharField(max_length=100, default="Logo SkyConnect")
+
+    def __str__(self):
+        return self.alt

a5388fbecc19094ecffdd251058385d30cc728c0
ACHIDAIME <mirabeautoureend@gmail.com>
2025-08-21 17:08:18 +0000

    test

--- skyconnect/settings.py
+++ skyconnect/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
@@ -90,7 +91,7 @@ DATABASES = {
 }
 """
 DATABASES = {
-    'default': dj_database_url.parse(env('_DATABASE_URL_'))
+    'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
 # Password validation
